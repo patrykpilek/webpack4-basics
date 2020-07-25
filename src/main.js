815ae230990509b4fd2469f057b4ad96868fa4a2
Patryk Pilek <patryk.pilek@gmail.com>
2020-07-25 17:41:07 +0100

    Diy webpack dev server with express middleware.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 require("@babel/runtime/regenerator");
+require("webpack-hot-middleware/client");
 require("./main.css");
 require("./index.html");
-
-let a = async args => {
-    const { a, b } = args;
-    await console.log("Hello from the future!");
-    console.log("Done");
-}
-
-a({ a: 1, b: 2 })
