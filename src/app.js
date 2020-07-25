2ba08eac436bd727b29a310827f50435d16be4da
Patryk Pilek <patryk.pilek@gmail.com>
2020-07-25 23:37:26 +0100

    React stateful reloading.

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Counter from "./counter";
+import { AppContainer } from "react-hot-loader";
+
+function render(Component) {
+    ReactDom.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('react-root')
+    )
+}
+
+render(Counter);
+
+if (module.hot) {
+    module.hot.accept("./counter.js", () => {
+        const NewCounter = require("./counter").default
+        render(NewCounter)
+    })
+}
